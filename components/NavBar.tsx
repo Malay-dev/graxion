1e353e20ee1bba831f673824cd372ccd3c2829c1
Malay Kumar <malaykumar2003@gmail.com>
2025-03-15 17:51:12 +0530

    Refactor: Fix dependencies and TeamSwitcher

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Search } from "./Search";
 import { MainNav } from "./MainNav";
 import { UserNav } from "./UserNav";
@@ -7,22 +8,26 @@ import TeamSwitcher from "./TeamSwitcher";
 import { ToggleTheme } from "./layout/toggle-theme";
 
 const NavBar = () => {
-  const pathname = window.location.pathname;
+  const [pathname, setPathname] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setPathname(window.location.pathname);
+    }
+  }, []);
 
   return (
     <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <ToggleTheme />
-            <TeamSwitcher />
-            {pathname !== "/dashboard" &&
-              <MainNav className="mx-6" />
-            }
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
+      <div className="flex h-16 items-center px-4">
+        <ToggleTheme />
+        <TeamSwitcher />
+        {pathname !== "/dashboard" && <MainNav className="mx-6" />}
+        <div className="ml-auto flex items-center space-x-4">
+          <Search />
+          <UserNav />
         </div>
+      </div>
+    </div>
   );
 };
 
--- components/TeamSwitcher.tsx
+++ components/TeamSwitcher.tsx
@@ -1,4 +1,3 @@
-// @ts-nocheck
 "use client"
 
 import * as React from "react"
@@ -73,9 +72,7 @@ type Team = (typeof groups)[number]["teams"][number]
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
-interface TeamSwitcherProps extends PopoverTriggerProps {}
-
-export default function TeamSwitcher({ className }: TeamSwitcherProps) {
+export default function TeamSwitcher({ className }: PopoverTriggerProps) {
   const [open, setOpen] = React.useState(false)
   const [showNewTeamDialog, setShowNewTeamDialog] = React.useState(false)
   const [selectedTeam, setSelectedTeam] = React.useState<Team>(
